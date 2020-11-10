f0a42f9a9f65e6b3c5f44ee099c2c8a1be3c08e9
Lemonier Cloe <cloelemonier12@gmail.com>
2020-11-10 13:54:37 +0100

    commit
    Signed-off-by: Lemonier Cloe <cloelemonier12@gmail.com>

---
+++ ESEO_TP_Serveur_maven_S9/src/main/java/com/dto/Ville.java
@@ -0,0 +1,17 @@
+package com.dto;
+
+
+public class Ville {
+	
+	public String codeCommune;
+
+	public String nomCommune;
+
+	public String codePostal;
+
+	public String libelleAcheminement;
+
+	public String ligne;
+	
+	
+}
